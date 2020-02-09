082777d5b3eb253f9624c8eaa1ed49fe7702fdbd
ghadd <48999054+ghadd@users.noreply.github.com>
2020-02-10 00:54:56 +0200

    Changed the whole algorithm
    Changed algorithm to find few most common colors, instead of mixing them. Gotta polish those colors probably(it depends on how fast is it gonna work) and visualize stuff.

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,8 +1,10 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <ctime>
+#include <opencv2/opencv.hpp> // working with images
+#include <iostream> // console input&output
+#include <vector> // std::vector
+#include <ctime> // time()
+#include <cmath> // sqrt, pow
+#include <map> // std::map;
+#include <utility> // std::pair;
 
 enum {
 	blue, green, red
@@ -10,8 +12,21 @@ enum {
 
 #define SCALER 10
 
-bool isNotBackground(uchar b, uchar g, uchar r) {
-	return true;
+bool inRange(cv::Vec3b stable, cv::Vec3b floating) {
+	double dist = sqrt(pow((uint)stable[blue] - (uint)floating[blue], 2) + \
+		pow((uint)stable[green] - (uint)floating[green], 2) + \
+		pow((uint)stable[red] - (uint)floating[red], 2));
+	return dist <= 128;
+}
+
+std::pair < bool, int > vecContainsInRange(std::vector<cv::Vec3b> iterable, cv::Vec3b obj) {
+	//cannot get any clue how can i use std::find_if()
+	for (int i = 0; i < iterable.size(); i++) {
+		if (inRange(iterable[i], obj)) {
+			return std::make_pair(true, i);
+		}
+	}
+	return std::make_pair(false, -1);
 }
 
 int main(int argc, char** argv) {
@@ -19,7 +34,7 @@ int main(int argc, char** argv) {
 	while (1) {
 		std::cin >> customPath;
 		if (customPath == "quit()") break;
-		std::string filePath = "C:\\Users\\User\\Desktop\\" + customPath;
+		std::string filePath = "C:\\Users\\User\\Desktop\\TestsCV\\" + customPath;
 		cv::Mat image = cv::imread(filePath);
 
 		if (image.empty()) {
@@ -31,70 +46,34 @@ int main(int argc, char** argv) {
 		cv::namedWindow(imageWindowName); // Create a window
 		cv::imshow(imageWindowName, image); // Show our image inside the created window.
 
-		std::fstream imageDataFile(filePath + ".pdt", std::fstream::out);
-
-		std::vector<unsigned long long> sumsOfColorValues(3, 0);
-		imageDataFile << image.rows << "x" << image.cols << std::endl;
 		unsigned long long totalValuedPixelsCount = image.rows * image.cols;
-		//for (int y = 0; y < image.rows; y++) {
-		//	for (int x = 0; x < image.cols; x++) {
-		//		cv::Vec3b color = image.at<cv::Vec3b>(cv::Point(x, y));
-		//		uchar blues = color[0];
-		//		uchar greens = color[1];
-		//		uchar reds = color[2];
 
-		//		if ((uint)blues >= 50 || (uint)greens >= 50 || (uint)reds >= 50) {
-		//			//50 to add track some intensity
-		//			sumsOfColorValues[blue] += (uint)blues;
-		//			sumsOfColorValues[green] += (uint)greens;
-		//			sumsOfColorValues[red] += (uint)reds;
-		//		}
-		//		else {
-		//			totalValuedPixelsCount--;
-		//		}
-		//		imageDataFile << (uint)reds << " " << (uint)greens << " " << (uint)blues << std::endl;
-		//	}
-		//}
-
-		// Let's try randomizing x and y for total of pixels over 10
 		std::srand(time(NULL));
-		unsigned long long totalCheckedPixelsCount = totalValuedPixelsCount / SCALER;
+		//some problems with std::map tho
+		std::vector<cv::Vec3b> popularColors;
+		std::vector<uint> popularColorsEncounters;
 		for (int i = 0; i < totalValuedPixelsCount / SCALER; i++) {
 			int x = std::rand() % image.cols;
 			int y = std::rand() % image.rows;
 			cv::Vec3b color = image.at<cv::Vec3b>(cv::Point(x, y));
-
-			uchar blues = color[0];
-			uchar greens = color[1];
-			uchar reds = color[2];
-
-			if ((uint)blues >= 10 || (uint)greens >= 10 || (uint)reds >= 10 && isNotBackground(blues, greens, reds)) {
-				sumsOfColorValues[blue] += (uint)blues;
-				sumsOfColorValues[green] += (uint)greens;
-				sumsOfColorValues[red] += (uint)reds;
+			if (vecContainsInRange(popularColors, color).first) {
+				popularColorsEncounters[vecContainsInRange(popularColors, color).second]++;
 			}
 			else {
-				totalCheckedPixelsCount--;
+				popularColors.push_back(color);
+				popularColorsEncounters.push_back(1);
 			}
 		}
 
-		size_t avgBlue = sumsOfColorValues[blue] / totalCheckedPixelsCount;
-		size_t avgGreen = sumsOfColorValues[green] / totalCheckedPixelsCount;
-		size_t avgRed = sumsOfColorValues[red] / totalCheckedPixelsCount;
-		cv::Scalar mainColor(avgBlue, avgGreen, avgRed);
-		imageDataFile << avgBlue << " " << avgGreen << " " << avgRed << std::endl;
-
-		imageDataFile.close();
-
-		cv::Mat mainColorImage(image.rows, image.cols, CV_8UC3, mainColor);
-
-		cv::String mainColorWindowName = customPath + " / mainColor"; //Name of the window
-		cv::namedWindow(mainColorWindowName); // Create a window
-		cv::imshow(mainColorWindowName, mainColorImage); // Show our image inside the created window.
+		for (int i = 0; i < popularColors.size(); i++) {
+			if (popularColorsEncounters[i] >= totalValuedPixelsCount / (20 * SCALER)) {
+				std::cout << popularColors[i] << " " << popularColorsEncounters[i] << std::endl;
+			}
+		}
 
 		cv::waitKey(0); // Wait for any keystroke in the window
-		cv::destroyWindow(mainColorWindowName); //destroy the created window
 		cv::destroyWindow(imageWindowName);
 	}
+
 	return 0;
 }
